f02009386333419dc72f709174cf63cf90c7481f
Phil Gengler <phil@pgengler.net>
2015-10-31 18:29:50 +0000

    Get cdr working

--- addon/helpers/cdr.js
+++ addon/helpers/cdr.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
-export function cdr() {
-  return [ 'b', 'c' ];
+export function cdr(val) {
+  if (Ember.isArray(val)) {
+    return val.slice(1);
+  }
+  return [ ];
 }
 
 export default Ember.Helper.helper(cdr);
---
+++ tests/unit/helpers/cdr-test.js
@@ -0,0 +1,22 @@
+import { cdr } from '../../../helpers/cdr';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | cdr');
+
+test('returns the remainder when the argument is an array', function(assert) {
+  let array = [ 'a', 'b', 'c' ];
+  let result = cdr(array);
+  assert.deepEqual(result, [ 'b', 'c' ]);
+});
+
+test('returns an empty array when the argument is a one-element array', function(assert) {
+  let array = [ 'a' ];
+  let result = cdr(array);
+  assert.deepEqual(result, [ ]);
+});
+
+test('returns an empty array when the argument is not an array', function(assert) {
+  let obj = { a: 1 };
+  let result = cdr(obj);
+  assert.deepEqual(result, [ ]);
+});
