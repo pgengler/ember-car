e0abcb3437f4dfbf1eb856892b22a891e34f68a9
Phil Gengler <phil@pgengler.net>
2015-10-31 19:00:07 +0000

    Make template helpers unpack params

--- app/helpers/car.js
+++ app/helpers/car.js
@@ -1,1 +1,8 @@
-export { default, car } from 'ember-car/helpers/car';
+import car from 'ember-car/helpers/car';
+
+export default function car(params) {
+  if (params.length !== 1) {
+    return null;
+  }
+  return car(params[0]);
+}
--- app/helpers/cdr.js
+++ app/helpers/cdr.js
@@ -1,1 +1,8 @@
-export { default, cdr } from 'ember-car/helpers/cdr';
+import cdr from 'ember-car/helpers/car';
+
+export default function cdr(params) {
+  if (params.length !== 1) {
+    return null;
+  }
+  return cdr(params[0]);
+}
